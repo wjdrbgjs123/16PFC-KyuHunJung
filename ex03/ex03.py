e96305c3d38f1cf35c8f9ef8730320745d1d3300
KyuHunJung <wjdrbgjs123@naver.com>
2016-03-22 12:54:57 +0900

    added ex03.py/ 연산

---
+++ ex03/ex03.py
@@ -0,0 +1,27 @@
+# -*-coding:cp949
+# http://http://learnpythonthehardway.org/book/ex3.html
+# 한글로 주석을 달때는 맨위에 주석 -*-coding:cp949 를 써라 코디 씨피 949
+# 정확한 계산을 위해서는 소수점을 적어줘라. 아니면 알아서 밑에 버린다
+print "I will now count my chickens"
+
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters" , 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "what is 3.0 + 2.0?", 3.0 + 2.0
+print "what is 5.0 - 7.0?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0<= -2.0
