fe57f9909f1cf6af336c1e03d22c07de60084d26
KyuHun Jung <wjdrbgjs123@naver.com>
2016-03-29 12:18:54 +0900

    added ex05.py study drills

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,13 +1,16 @@
-my_name = 'Zed A. Shaw'
+name = 'Zed A. Shaw'
 my_age = 35 # not a lie
-my_height = 74 # inches
+my_height_cm = 180
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
 my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
+print "Let's talk about %s." % name
+print "He's %d centimeters tall." % my_height_cm
+print "He's %d inches tall." % my_height_inch
 print "He's %d pounds heavy." % my_weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
@@ -15,4 +18,4 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    my_age, my_height_cm, my_weight, my_age + my_height_cm + my_weight)
