02e23542882f22234ffeb9ef4d8ec0c103f8bdc6
KyuHun Jung <wjdrbgjs123@naver.com>
2016-04-05 11:03:26 +0900

    added ex09.py

---
+++ ex09/ex09.py
@@ -0,0 +1,17 @@
+# -*-coding:cp949 한글 주석
+#  빽 슬레쉬는 궮� 대신 쓰면됨
+# 굈 은 여러줄을 나오게함 또한 """ 도 여러줄 나오게함
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan굈Feb굈Mar굈Apr굈May굈Jun굈Jul굈Aug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
