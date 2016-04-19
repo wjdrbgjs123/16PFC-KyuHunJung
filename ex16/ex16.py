6b056cd46e2c3ce6d46c6317676ea30fc4bc87ae
KyuHunJung <wjdrbgjs123@naver.com>
2016-04-19 10:52:42 +0900

    ex16.py 내용수정

--- ex16/ex16.py
+++ ex16/ex16.py
@@ -24,7 +24,7 @@ print ("I'm going to write these to the file.")
 
 target.write(line1)
 target.write("\n")
-target.wirte(line2)
+target.write(line2)
 target.write("\n")
 target.write(line3)
 target.write("\n")
