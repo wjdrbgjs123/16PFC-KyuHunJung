abf65fa63e05fc3a366ffc6a9bfe64e5a83c8456
CAD Client <CAD Client>
2016-05-24 11:36:44 +0900

    added ex34.py

---
+++ ex34/ex34.py
@@ -0,0 +1,13 @@
+# -*-coding:cp949
+# coding: utf-8
+
+animals = ['°õ', 'È£¶ûÀÌ', 'Æë±Ï', '¾ó·è¸»']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+print("bear = %s" % bear)
+print("tiger = %s" % tiger)
+print("penguin = %s" % penguin)
+print("zebra = %s" % zebra)
